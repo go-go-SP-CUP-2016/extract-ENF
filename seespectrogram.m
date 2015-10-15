b34b82c79e985538c6c1cfe9d7089113a2be0a90
JackingChen <cow12369@gmail.com>
2015-10-15 09:55:44 +0800

    see spectrogram update
    haven’t deal with frequency resolution

--- seespectrogram.m
+++ seespectrogram.m
@@ -1,17 +1,27 @@
-[y,Fs] = audioread('./Train_Grid_G_P1.wav');
+[y,Fs] = audioread('./test.wav');
 
 %spectrogram(y,512,120,1024,Fs);
-f=fir1(32,[49*2/Fs 51*2/Fs]);
-f1=fir1(32,70*2/Fs,'low');
-y_fil=conv(y,f1,'same');
-yd=downsample(y_fil,8,0);
-figure(4)
-spectrogram(yd,1024/4,60,512*4,Fs/8);
+f=fir1(32,[59*2/Fs 61*2/Fs]);% filter at sample frequency 59~61
+f1=fir1(32,70*2/Fs,'low');%low pass filter for down sampling
+y_fil=conv(y,f,'same');% filter the raw fignal
 
-a=conv(yd,f,'same');
+a=conv(y_fil,f1,'same');% low pass the original signal before downsampling
+yd=downsample(a,8,0);% dowmsample to around 125hz
+
+Fs_d=Fs/8;% downsample rate
+
+%figure(4)
+%spectrogram(yd,1024/4,60,512*4,Fs/8);
+
+
 figure(2);
-spectrogram(a,1024/4,60,512*4,Fs/8);
-ss=spectrogram(a,1024/4,60,512*4,Fs/8);
-
+spectrogram(yd,1024,60,512,Fs_d);% show sownsampled spectrogram
+ss=spectrogram(yd,1024,60,512,Fs_d);
+[row col]=find(ss>0.1);% get the interested points
+x=1:1:length(row);
 figure(3)
-imshow( abs(ss(end:-1:1, :)) );
+plot(x,row);% ENF signal plot
+
+
+%figure(3)
+%imshow( abs(ss(1:1:end, :)) );
